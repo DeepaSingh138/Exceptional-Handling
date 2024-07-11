61a0f048866700d9cf3f3dbf0157895d6fee37ff
DeepaSingh138 <deepadelight001@gmail.com>
2024-07-11 18:02:26 +0530

    Exceptional Handling

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ExcsptionalHandling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] arr = new int[5];
+
+            Console.WriteLine("Enter the array elements");
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+
+                }
+                Console.WriteLine("Entered list of Array is:");
+
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (Exception Indexoutofrange)
+            {
+                Console.WriteLine("out of range");
+            }
+            finally
+            {
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
